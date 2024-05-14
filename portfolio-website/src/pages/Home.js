37f242fc567e1cf0968f0a68ca7274e7412447bd
YoKarson <lantk991@gmail.com>
2024-05-13 17:01:59 -0700

    updated home

--- portfolio-website/src/pages/Home.js
+++ portfolio-website/src/pages/Home.js
@@ -1,7 +1,41 @@
 import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GithubIcon from "@material-ui/icons/GitHub";
+import EmailIcon from "@material-ui/icons/Email";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My Name is Karson</h2>
+        <div className="prompt">
+          <p>A software developer</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span> HTML, CSS, JavaScript, React</span>
+          </li>
+
+          <li className="item">
+            <h2> Back-End</h2>
+            <span> NodeJS, MySQL</span>
+          </li>
+
+          <li className="item">
+            <h2> Languages</h2>
+            <span> JavaScript, Java, C, C++, </span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
