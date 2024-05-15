a48f85f61229771538a99186f1923d1e4a7ae3c6
YoKarson <lantk991@gmail.com>
2024-05-15 12:21:25 -0700

    add imgs + css for projects

--- portfolio-website/src/pages/Projects.js
+++ portfolio-website/src/pages/Projects.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
+import ExpenseTrackerImg from "../assets/ExpenseTrackerImg.jpg";
+import QuizAppImg from "../assets/QuizAppImg.png";
 
+import "../styles/Project.css";
 
 function Projects() {
   return (
     <div className="projects">
       <h1> My Personal Projects</h1>
       <div className="projectList"></div>
-      <ProjectItem name="social" image={}/>
+      <ProjectItem name="social" image={ExpenseTrackerImg} />
+      <ProjectItem name="social" image={QuizAppImg} />
     </div>
   );
 }
